e8c2247edb5e10ebcb79b575d1334176b12c3daa
Ryjedd <rlambert92@live.ca>
2017-07-09 22:31:00 -0400

    menu controls
    menu links to appropriate locations on page, returns menu

--- js/menu-controls.js
+++ js/menu-controls.js
@@ -2,6 +2,8 @@ var menuOpen = false;
 var $navCtrl = $('.nav-control');
 var $navBulk = $('.nav');
 var $main = $('.main');
+var $navBtn = $('.nav-button');
+var $mainBtn = $('.main-button');
 
 $(document).ready(function(){
 	
@@ -17,6 +19,65 @@ $(document).ready(function(){
 			$navBulk.animate({right:'-=15em'}, 500);
 			menuOpen = false;
 		}
+	});
+	
+	$navCtrl.hover(
+		function(){
+			$navCtrl.animate({'backgroundColor':'#444444'}, 150);
+		
+		},
+		function(){
+			$navCtrl.animate({'backgroundColor':'#222222'}, 150);
+		
+		},
+	);
+	
+	$navBtn.hover(
+		function(){
+			$(this).animate({'backgroundColor':'#666666'}, 100);
+		
+		},
+		function(){
+			$(this).animate({'backgroundColor':'#555555'}, 100);
+		
+		},
+	);
+	
+	$mainBtn.hover(
+		function(){
+			$(this).animate({'backgroundColor':'#AA6666'}, 100);
+		
+		},
+		function(){
+			$(this).animate({'backgroundColor':'#995555'}, 100);
+		
+		},
+	);
+	
+	$navBtn.click(function(){
+		$navCtrl.animate({right:'-=15em'}, 500);
+		$main.animate({right:'-=15em'}, 500);
+		$navBulk.animate({right:'-=15em'}, 500);
+		menuOpen = false;
+		
+		var $href = $(this).attr('href');
+		var $anchor = $($href).offset();
+		$('body').animate({ scrollTop: $anchor.top  - 80});
+		return false;
+	});
+	
+	$mainBtn.click(function(){
+		$navCtrl.animate({right:'-=15em'}, 500);
+		$main.animate({right:'-=15em'}, 500);
+		$navBulk.animate({right:'-=15em'}, 500);
+		menuOpen = false;
+		
+		var $href = $(this).attr('href');
+		var $anchor = $($href).offset();
+		$('body').animate({ scrollTop: $anchor.top - 100});
+		return false;
+		
+		
 	});
 	
 });
